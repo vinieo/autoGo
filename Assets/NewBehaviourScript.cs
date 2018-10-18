9575a97f81fdcc22088816483f132d4d01e27bfa
vinieo <374863506@qq.com>
2018-10-19 00:29:54 +0800

    game

---
+++ Assets/NewBehaviourScript.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NewBehaviourScript : MonoBehaviour {
+	private UnityEngine.AI.NavMeshAgent aa;
+	public Transform bb;
+	// Use this for initialization
+	void Start () {
+		aa = gameObject.GetComponent<UnityEngine.AI.NavMeshAgent>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		aa.SetDestination(bb.position);
+	}
+}
